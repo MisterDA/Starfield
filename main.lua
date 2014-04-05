34b98a91672d51e9fda73077a32449fee6b7dd5d
Antonin Décimo <antonin.decimo@gmail.com>
2014-04-05 20:34:06 +0200

    Add z-scaling, brigthness, config, and minor improvements

--- main.lua
+++ main.lua
@@ -1,32 +1,42 @@
-function love.load()
-    X, Y = love.graphics.getWidth() / 2, love.graphics.getHeight() / 2
-    maxStars = 63
+function love.load ()
+    W, H = love.graphics.getDimensions()
+    X, Y = W/2, H/2
+    maxStars = 1024
     Stars = {}
-    for i=0, maxStars do
-        Stars[i] = {
-            x = X,
-            y = Y,
-            vx = math.random() * 4 - 2,
-            vy = math.random() * 4 - 2
-        }
+
+    function reset (s)
+        s.x = math.random(-W*3, W*3)
+        s.y = math.random(-H*3, H*3)
+        s.z = math.random(100, 1000)
+        s.sx = 0
+        s.sy = 0
+    end
+
+    for i=1, maxStars do
+        Stars[i] = {}
+        reset(Stars[i])
     end
 end
 
-function love.draw()
-    for i=0, maxStars do
+function love.draw ()
+    for i=1, maxStars do
         s = Stars[i]
-        s.x = s.x + s.vx
-        s.y = s.y + s.vy
 
-        if s.x < 0 or s.x > love.graphics.getWidth() or
-           s.y < 0 or s.y > love.graphics.getHeight() then
-            s.x = X
-            s.y = Y
-            s.vx = math.random() * 4 - 2
-            s.vy = math.random() * 4 - 2
+        s.z = s.z - 5
+
+        s.sx = s.x / s.z * 100 + X
+        s.sy = s.y / s.z * 100 + Y
+
+        if s.sx < 0 or s.sx > W or
+           s.sy < 0 or s.sy > H or
+           s.z < 1 then
+            reset(s)
         end
 
-        love.graphics.point(s.x, s.y)
+        b = 350 - s.z * 0.35
+        if b > 255 then b = 255 end
+        love.graphics.setColor(b, b, b)
+        love.graphics.point(s.sx, s.sy)
     end
 end
 
@@ -34,4 +44,9 @@ function love.keypressed (k)
     if k == 'escape' then
         love.event.quit()
     end
+end
+
+function love.resize (w, h)
+    W, H = w, h
+    X, Y = W/2, H/2
 end
